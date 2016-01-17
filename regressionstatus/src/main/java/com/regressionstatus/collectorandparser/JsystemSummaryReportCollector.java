d0caff2338572cf9223c49cd6ca94f982d7c28bd
Jonah Tornovsky <jtornovsky@vasonanetworks.com>
2016-01-17 10:41:57 +0200

    fixing closing stream in collector and adding some java doc

--- regressionstatus/src/main/java/com/regressionstatus/collectorandparser/JsystemSummaryReportCollector.java
+++ regressionstatus/src/main/java/com/regressionstatus/collectorandparser/JsystemSummaryReportCollector.java
@@ -11,6 +11,12 @@ import java.nio.file.StandardCopyOption;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * Class used to copy jsystem's summary.html report from the remote machine that runs one of regression scenario 
+ * to the local machine to prepare before calculating report's statistics 
+ * @author jtornovsky
+ *
+ */
 @Component("jsystemSummaryReportCollector")
 public class JsystemSummaryReportCollector implements DataCollector {
 
@@ -18,16 +24,29 @@ public class JsystemSummaryReportCollector implements DataCollector {
 	public void collectDataAtRemoteStation(String remoteStationIpaddress, String jsystemReportSourceFile, String jsystemReportTargetFile) {
 
 		URL jsystemSummaryReportUrl = null;
+		
+		// building url to copy summary.html report
 		try {
 			jsystemSummaryReportUrl = new URL("http://" + remoteStationIpaddress + "/" + jsystemReportSourceFile);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
-		try (InputStream in = jsystemSummaryReportUrl.openStream()) {
+		
+		// copying summary.html report from remote to local 
+		InputStream in = null;
+		try  {
+			in = jsystemSummaryReportUrl.openStream();
 			Path targetPath = Paths.get(jsystemReportTargetFile);
 		    Files.copy(in, targetPath, StandardCopyOption.REPLACE_EXISTING);
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			 // Close stream here
+			try {
+				in.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
